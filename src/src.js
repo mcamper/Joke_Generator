5a0edf9ff34e17338c3839a69ce9cf680d945c35
mariacamper <mariathetech180@gmail.com>
2025-01-28 22:47:48 -0500

    Update src.js. Captured response

--- src/src.js
+++ src/src.js
@@ -23,8 +23,11 @@ apiKey = "77ao6ba83c370f60fbc94613061ab8t5";
 apiUrl = "https://official-joke-api.appspot.com/random_joke";
 
 axios.get(apiUrl).then(showJoke);
+console.log(apiUrl);
 
 function showJoke(response) {
     console.log(response.data.setup);
-    console.log(response.data.setup.punchline);
+    console.log(response.data.punchline);
+
+    
 }
